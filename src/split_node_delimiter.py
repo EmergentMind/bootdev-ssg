9277c4bd741e66cb0ef816dd63660b05c6435527
emergentmind <emergentmind@users.noreply.github.com>
2026-03-10 15:43:25 -0600

    feat: split delimiter assignment

---
+++ src/split_node_delimiter.py
@@ -0,0 +1,28 @@
+from textnode import TextNode, TextType
+
+def split_node_delimiter(old_nodes, delimiter, text_type):
+    new_nodes = []
+    for node in old_nodes:
+        if node.text_type is not TextType.PLAIN:
+            new_nodes.append(node)
+            continue
+        text_parts = node.text.split(delimiter)
+        if is_valid_md(text_parts):
+            part_nodes = []
+            for i in range(0, len(text_parts)):
+                part_type = TextType.PLAIN
+                if (i+1) % 2 == 0:
+                    # every second text_part is of type text_type
+                    part_type = text_type
+                part_nodes.append(TextNode(text_parts[i], part_type))
+            new_nodes.extend(part_nodes)
+    return new_nodes
+
+# an even number of node_parts implies the delimiter(s) was not paired
+def is_valid_md(list):
+    if len(list) % 2 != 0:
+        return True
+    else:
+        raise Excpetion(
+            "Encountered invalid markdown while splitting the node"
+        )
---
+++ src/test_split_node_delimiter.py
@@ -0,0 +1,121 @@
+import unittest
+from textnode import TextNode, TextType
+from split_node_delimiter import split_node_delimiter
+
+class TestTextNode(unittest.TestCase):
+    def test_plain_text(self):
+        node = TextNode("this is some text without nested elements",
+                        TextType.PLAIN)
+        node2 = TextNode("this is some other text without nested elements",
+                        TextType.PLAIN)
+        new_nodes = split_node_delimiter([node, node2], "**", TextType.BOLD)
+        self.assertEqual(new_nodes, [node, node2])
+        self.assertEqual(new_nodes[0].text,"this is some text without nested elements")
+        self.assertEqual(new_nodes[1].text,"this is some other text without nested elements")
+
+    def test_non_plain_text(self):
+        node = TextNode("_this is some text without nested elements_",
+                        TextType.ITALIC)
+        node2 = TextNode("_this is some other text without nested elements_",
+                        TextType.ITALIC)
+        new_nodes = split_node_delimiter([node, node2], "**", TextType.BOLD)
+        self.assertEqual(new_nodes, [node, node2])
+        self.assertEqual(new_nodes[0].text,"_this is some text without nested elements_")
+        self.assertEqual(new_nodes[1].text,"_this is some other text without nested elements_")
+
+    def test_bold_text(self):
+        node = TextNode("this is some text with a nested **bold text** element",
+                        TextType.PLAIN)
+        node2 = TextNode("this is some other text without nested elements",
+                        TextType.PLAIN)
+        new_nodes = split_node_delimiter([node, node2], "**", TextType.BOLD)
+        self.assertNotEqual(new_nodes, [node, node2])
+        self.assertEqual(new_nodes[0].text, "this is some text with a nested ")
+        self.assertEqual(new_nodes[0].text_type, TextType.PLAIN)
+        self.assertEqual(new_nodes[1].text, "bold text")
+        self.assertEqual(new_nodes[1].text_type, TextType.BOLD)
+        self.assertEqual(new_nodes[2].text, " element")
+        self.assertEqual(new_nodes[2].text_type, TextType.PLAIN)
+        self.assertEqual(new_nodes[3].text, "this is some other text without nested elements")
+        self.assertEqual(new_nodes[3].text_type, TextType.PLAIN)
+
+    def test_double_bold_text(self):
+        node = TextNode("this is some text with **two** nested **bold text** elements",
+                        TextType.PLAIN)
+        new_nodes = split_node_delimiter([node], "**", TextType.BOLD)
+        self.assertNotEqual(new_nodes, [node])
+        self.assertEqual(new_nodes[0].text, "this is some text with ")
+        self.assertEqual(new_nodes[0].text_type, TextType.PLAIN)
+        self.assertEqual(new_nodes[1].text, "two")
+        self.assertEqual(new_nodes[1].text_type, TextType.BOLD)
+        self.assertEqual(new_nodes[2].text, " nested ")
+        self.assertEqual(new_nodes[2].text_type, TextType.PLAIN)
+        self.assertEqual(new_nodes[3].text, "bold text")
+        self.assertEqual(new_nodes[3].text_type, TextType.BOLD)
+        self.assertEqual(new_nodes[4].text, " elements")
+        self.assertEqual(new_nodes[4].text_type, TextType.PLAIN)
+
+    def test_italic_text(self):
+        node = TextNode("this is some text with a nested _italic text_ element",
+                        TextType.PLAIN)
+        node2 = TextNode("this is some other text without nested elements",
+                        TextType.PLAIN)
+        new_nodes = split_node_delimiter([node, node2], "_", TextType.ITALIC)
+        self.assertNotEqual(new_nodes, [node, node2])
+        self.assertEqual(new_nodes[0].text, "this is some text with a nested ")
+        self.assertEqual(new_nodes[0].text_type, TextType.PLAIN)
+        self.assertEqual(new_nodes[1].text, "italic text")
+        self.assertEqual(new_nodes[1].text_type, TextType.ITALIC)
+        self.assertEqual(new_nodes[2].text, " element")
+        self.assertEqual(new_nodes[2].text_type, TextType.PLAIN)
+        self.assertEqual(new_nodes[3].text, "this is some other text without nested elements")
+        self.assertEqual(new_nodes[3].text_type, TextType.PLAIN)
+
+    def test_double_italic_text(self):
+        node = TextNode("this is some text with _two_ nested _italic text_ elements",
+                        TextType.PLAIN)
+        new_nodes = split_node_delimiter([node], "_", TextType.ITALIC)
+        self.assertNotEqual(new_nodes, [node])
+        self.assertEqual(new_nodes[0].text, "this is some text with ")
+        self.assertEqual(new_nodes[0].text_type, TextType.PLAIN)
+        self.assertEqual(new_nodes[1].text, "two")
+        self.assertEqual(new_nodes[1].text_type, TextType.ITALIC)
+        self.assertEqual(new_nodes[2].text, " nested ")
+        self.assertEqual(new_nodes[2].text_type, TextType.PLAIN)
+        self.assertEqual(new_nodes[3].text, "italic text")
+        self.assertEqual(new_nodes[3].text_type, TextType.ITALIC)
+        self.assertEqual(new_nodes[4].text, " elements")
+        self.assertEqual(new_nodes[4].text_type, TextType.PLAIN)
+
+    def test_code_text(self):
+        node = TextNode("this is some text with a nested `code text` element",
+                        TextType.PLAIN)
+        node2 = TextNode("this is some other text without nested elements",
+                        TextType.PLAIN)
+        new_nodes = split_node_delimiter([node, node2], "`", TextType.CODE)
+        self.assertNotEqual(new_nodes, [node, node2])
+        self.assertEqual(new_nodes[0].text, "this is some text with a nested ")
+        self.assertEqual(new_nodes[0].text_type, TextType.PLAIN)
+        self.assertEqual(new_nodes[1].text, "code text")
+        self.assertEqual(new_nodes[1].text_type, TextType.CODE)
+        self.assertEqual(new_nodes[2].text, " element")
+        self.assertEqual(new_nodes[2].text_type, TextType.PLAIN)
+        self.assertEqual(new_nodes[3].text, "this is some other text without nested elements")
+        self.assertEqual(new_nodes[3].text_type, TextType.PLAIN)
+
+    def test_double_code_text(self):
+        node = TextNode("this is some text with `two` nested `code text` elements",
+                        TextType.PLAIN)
+        new_nodes = split_node_delimiter([node], "`", TextType.CODE)
+        self.assertNotEqual(new_nodes, [node])
+        self.assertEqual(new_nodes[0].text, "this is some text with ")
+        self.assertEqual(new_nodes[0].text_type, TextType.PLAIN)
+        self.assertEqual(new_nodes[1].text, "two")
+        self.assertEqual(new_nodes[1].text_type, TextType.CODE)
+        self.assertEqual(new_nodes[2].text, " nested ")
+        self.assertEqual(new_nodes[2].text_type, TextType.PLAIN)
+        self.assertEqual(new_nodes[3].text, "code text")
+        self.assertEqual(new_nodes[3].text_type, TextType.CODE)
+        self.assertEqual(new_nodes[4].text, " elements")
+        self.assertEqual(new_nodes[4].text_type, TextType.PLAIN)
+
